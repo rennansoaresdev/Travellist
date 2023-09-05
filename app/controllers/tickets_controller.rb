04d0debf14d5e5ea1631d5076e12dbca8cae1221
cesar ramalho <cesar.ramalho@hotmail.com>
2023-09-05 15:03:21 -0300

    ok

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,31 @@
 class TicketsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
   def new
     @ticket = Ticket.new
+    @trip = Trip.find(params[:trip_id])
   end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    @ticket.user = current_user
+    @ticket.trip_id = params[:trip_id]
+
+    if @ticket.save
+      redirect_to ticket_path(@ticket), notice: 'Ticket was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:url, :start_time, :end_time, :origin, :destination, :seat, :gate, :price, :company_name)
+  end
+
 end
