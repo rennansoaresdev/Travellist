1ae8e6652bb187da1ffbd8cd02e8536db118691c
cesar ramalho <cesar.ramalho@hotmail.com>
2023-09-07 11:33:36 -0300

    alteração de coluna type para category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_203047) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_142526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_203047) do
     t.datetime "checkout_time"
     t.decimal "price", precision: 8, scale: 2
     t.integer "capacity"
-    t.string "type"
+    t.string "category"
     t.string "name"
     t.string "address"
     t.bigint "user_id", null: false
