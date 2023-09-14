aeba41b1c186b05a43a0fba2872f52ce98c94adf
Weslley Alencar <weslley.96@outlook.com>
2023-09-13 21:06:05 -0300

    Cards inseridos na homepage, faltam duas sections

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,7 +3,10 @@ class TripsController < ApplicationController
 
   def index
     # Necessário adicionar viagens criadas por OUTRAS pessoas, mas que o current_user pertença ao grupo
-    @trips = Trip.joins(:members).where('owner_id = :user OR members.user_id = :user', user: current_user)
+    # @trips = Trip.where(owner: current_user)
+    @trips = Trip.left_outer_joins(:members)
+                 .where('owner_id = :user OR members.user_id = :user', user: current_user)
+                 .distinct
   end
 
   def show
